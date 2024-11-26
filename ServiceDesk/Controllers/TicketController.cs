0b612e0ae6fb53e62aa85efb710d6ef4b0d3c5df
crise <crise@CRISTIANLEGION>
2024-11-26 14:04:53 -0300

    Agregar archivos de proyecto.

---
+++ ServiceDesk/Controllers/TicketController.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using ServiceDesk.Models;
+using ServiceDesk.ViewModels;
+
+namespace ServiceDesk.Controllers
+{
+    public class TicketController : Controller
+    {
+        private TicketsContext context = new TicketsContext();
+        // GET: Ticket
+        public ActionResult Index()
+        {
+           
+                var listado = context.Ticket;
+                return View(listado);
+            
+        }
+        [HttpGet]
+        public ActionResult Nuevo()
+        {
+            var viewmodel = new TicketViewModels();
+            viewmodel.Responsables = context.Responsable.ToList();
+            viewmodel.Estados = context.Estado.ToList();   
+            viewmodel.Usuarios = context.Usuario.ToList();
+            return View(viewmodel);
+        }
+
+        [HttpPost]
+        public ActionResult Nuevo(Ticket ticket)
+        {
+            context.Ticket.Add(ticket);
+            context.SaveChanges();
+            return RedirectToAction("Index"); 
+        }
+        [HttpGet]
+        public ActionResult Detalle (int id)
+        {
+            var viewModel = new TicketViewModels();
+            viewModel.Ticket = context.Ticket.FirstOrDefault(x => x.Id == id);
+            return View(viewModel);
+
+        }
+
+        [HttpGet]
+        public ActionResult Actualizar(int id)
+        {
+            var viewModel = new TicketViewModels();
+            viewModel.Ticket = context.Ticket.FirstOrDefault(x => x.Id == id);
+            viewModel.Responsables = context.Responsable.ToList();
+            viewModel.Estados = context.Estado.ToList();
+            viewModel.Usuarios = context.Usuario.ToList();
+            return View(viewModel);
+
+        }
+
+        [HttpPost]
+        public ActionResult Actualizar(Ticket ticket)
+        {
+            context.Entry(ticket).State = System.Data.Entity.EntityState.Modified;
+            context.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        [HttpGet]
+        public ActionResult Eliminar(int id)
+        {
+            var ticket = context.Ticket.FirstOrDefault(x => x.Id == id);
+            context.Ticket.Remove(ticket);
+            context.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+    }
+}
---
+++ ServiceDesk/ViewModels/TicketViewModels.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using ServiceDesk.Models;
+
+namespace ServiceDesk.ViewModels
+{
+    public class TicketViewModels
+    {
+        public Ticket Ticket { get; set; }
+
+        public List<Responsable> Responsables { get; set; }
+        public List<Usuario> Usuarios{ get; set; }
+        public List<Estado> Estados { get; set; }
+
+    }
+}
